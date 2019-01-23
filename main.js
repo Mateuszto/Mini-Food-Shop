82d1d630d8ae2af83934a3a919f46ed5f2c0046a
Mateuszto <38840598+Mateuszto@users.noreply.github.com>
2019-01-23 21:53:35 +0100

    Another fixes - Filters
    Add filters and hover on item effect.

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const fastFood_btn = document.querySelector('.fastFood');
 const polishFood_btn = document.querySelector('.polishFood');
 const germanFood_btn = document.querySelector('.germanFood');
 
+const filters = document.querySelector('.clear__wrapper');
+
 const items = document.querySelectorAll('.main__item');
 const itemsArray = [...items]
 
@@ -10,11 +12,49 @@ const fastFoodSort = (e) => {
 
     itemsArray.forEach(el => {
         if (el.dataset.food === 'fast') {
-            console.log("Sort: Fast-foods");
+            el.style.display = "flex";
         } else {
             el.style.display = "none";
         }
     });
+    filters.classList.add('clear__active')
+}
+const polishFoodSort = (e) => {
+    e.preventDefault()
+
+    itemsArray.forEach(el => {
+        if (el.dataset.food === 'polish') {
+            el.style.display = "flex";
+        } else {
+            el.style.display = "none";
+        }
+    });
+    filters.classList.add('clear__active')
+}
+const germanFoodSort = (e) => {
+    e.preventDefault()
+
+    itemsArray.forEach(el => {
+        if (el.dataset.food === 'german') {
+            el.style.display = "flex";
+        } else {
+            el.style.display = "none";
+        }
+    });
+    filters.classList.add('clear__active')
 }
 
+const closeBtn = document.querySelector('.clearPlus');
+
+const closeFilters = () => {
+    itemsArray.forEach(el => {
+        el.style.display = "flex";
+    });
+
+    filters.classList.remove('clear__active')
+}
+
+closeBtn.addEventListener('click', closeFilters);
+germanFood_btn.addEventListener('click', germanFoodSort);
+polishFood_btn.addEventListener('click', polishFoodSort);
 fastFood_btn.addEventListener('click', fastFoodSort);
