cbfb825110488bae96f61ef63d28a0134044d8c7
Mateuszto <38840598+Mateuszto@users.noreply.github.com>
2019-01-24 20:00:52 +0100

    Finish project
    Finish project - Sorting, add some buttons and hovers. Minimize javascript sorting function.

--- main.js
+++ main.js
@@ -1,61 +1,24 @@
-const fastFood_btn = document.querySelector('.fastFood');
-const polishFood_btn = document.querySelector('.polishFood');
-const germanFood_btn = document.querySelector('.germanFood');
+//Filter Function
 const closeBtn = document.querySelector('.clearPlus');
 const filters = document.querySelector('.clear__wrapper');
 
 const items = document.querySelectorAll('.main__item');
 const itemsArray = [...items]
 
-const fastFoodSort = (e) => {
-    e.preventDefault()
+const sortFunction = (itemName) => {
+    event.preventDefault()
 
     itemsArray.forEach(el => {
-        if (el.dataset.food === 'fast') {
+        if (el.dataset.food === itemName || itemName === 'clear_filters') {
             el.style.display = "flex";
         } else {
             el.style.display = "none";
         }
     });
-    filters.classList.add('clear__active')
 }
-const polishFoodSort = (e) => {
-    e.preventDefault()
-
-    itemsArray.forEach(el => {
-        if (el.dataset.food === 'polish') {
-            el.style.display = "flex";
-        } else {
-            el.style.display = "none";
-        }
-    });
-    filters.classList.add('clear__active')
-}
-const germanFoodSort = (e) => {
-    e.preventDefault()
-
-    itemsArray.forEach(el => {
-        if (el.dataset.food === 'german') {
-            el.style.display = "flex";
-        } else {
-            el.style.display = "none";
-        }
-    });
-    filters.classList.add('clear__active')
-}
-
-
-const closeFilters = () => {
-    itemsArray.forEach(el => {
-        el.style.display = "flex";
-    });
-
-    filters.classList.remove('clear__active')
-}
-
-
-//Listeners
-closeBtn.addEventListener('click', closeFilters);
-germanFood_btn.addEventListener('click', germanFoodSort);
-polishFood_btn.addEventListener('click', polishFoodSort);
-fastFood_btn.addEventListener('click', fastFoodSort);
+document.body.addEventListener('click', event => {
+    const elementClicked = event.target;
+    if (elementClicked.classList.contains('filterFood')) {
+        sortFunction(elementClicked.dataset.food);
+    }
+});
